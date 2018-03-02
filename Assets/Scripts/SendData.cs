52262fc9168aead5cd56d2b3950ee193ebc6830d
al285785 <al285785@uji.es>
2018-03-02 13:17:57 +0100

    App del profesor con Firebase (1)

--- Assets/Scripts/SendData.cs
+++ Assets/Scripts/SendData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Firebase;
 
 public class SendData : MonoBehaviour {
@@ -10,8 +11,12 @@ public class SendData : MonoBehaviour {
 	public InputField readData;
 	public InputField q1Data;
 
+	public InputField gInput;
+	Button buttonPressed;
+
 	public InputField consolaAndroid;
 	public int check = 0;
+	public string grupoElegido = "";
 
 	private Firebase.Database.DatabaseReference mDatabase;
 	private Firebase.Database.DataSnapshot mDataSnapshot;
@@ -21,44 +26,65 @@ public class SendData : MonoBehaviour {
 	private string newDato;
 	private string newQ1;
 
+	const string glyphs= "abcdefghijklmnopqrstuvwxyz0123456789";
+	public string userID;
+
 	void Start(){
+		int charAmount = Random.Range(20, 35); //set those to the minimum and maximum length of your string
+		for(int i=0; i<charAmount; i++)
+		{
+			userID += glyphs[Random.Range(0, glyphs.Length)];
+		}
+
 		mDatabase = Firebase.Database.FirebaseDatabase.GetInstance (urlDatabase).GetReference("/EscapeRoom");
-		mDatabase.Child ("question1").SetValueAsync (questionChecked);
+		mDatabase.Child (userID).Child ("Questions").Child ("question1").SetValueAsync (questionChecked);
 	}
 
 	void Update(){
-
 		Firebase.Database.FirebaseDatabase.GetInstance (urlDatabase).GetReference("/EscapeRoom").ValueChanged += HandleValueChanged;
 
 		consolaAndroid.text = check.ToString();
 
 		if (mDataSnapshot != null) {
 			consolaAndroid.image.color = Color.green;
-			if (mDataSnapshot.Child ("data").GetValue (true) != null) {
-				newDato = mDataSnapshot.Child ("data").GetValue (true).ToString ();
+			if (mDataSnapshot.Child(userID).Child ("data").GetValue (true) != null) {
+				newDato = mDataSnapshot.Child(userID).Child ("data").GetValue (true).ToString ();
 				readData.text = newDato;
 			}
 
 			if (newDato == "hola")
 				readData.image.color = Color.green;
 		
-			if (mDataSnapshot.Child ("question1").GetValue (true) != null) {
-				newQ1 = mDataSnapshot.Child ("question1").GetValue (true).ToString ();
+			if (mDataSnapshot.Child(userID).Child ("Questions").Child ("question1").GetValue (true) != null) {
+				newQ1 = mDataSnapshot.Child(userID).Child ("Questions").Child ("question1").GetValue (true).ToString ();
 				if (newQ1 == "true")
 					q1Data.image.color = Color.yellow;
 				else
 					q1Data.image.color = Color.red;
 			}
 		}
+
+		if (grupoElegido != "") {
+			gInput.text = grupoElegido;
+			gInput.image.color = Color.green;
+		}
 	}
 
 	public void SendButtonPressed(){
-		mDatabase.Child ("data").SetValueAsync (data.text);
+		mDatabase.Child(userID).Child ("data").SetValueAsync (data.text);
 	}
 
 	public void CheckButtonPressed(){
 		questionChecked = "true";
-		mDatabase.Child ("question1").SetValueAsync (questionChecked);
+		mDatabase.Child(userID).Child ("Questions").Child ("question1").SetValueAsync (questionChecked);
+	}
+
+	public void CheckGrupo(){
+		if (grupoElegido == "") {
+			grupoElegido = EventSystem.current.currentSelectedGameObject.name;
+			mDatabase.Child(userID).Child("mi Grupo").SetValueAsync (grupoElegido);
+			mDatabase.Child ("Grupos").Child (grupoElegido).Child ("userID").SetValueAsync (userID);
+		}
 	}
 
 	void HandleValueChanged(object sender, Firebase.Database.ValueChangedEventArgs args){
@@ -71,5 +97,4 @@ public class SendData : MonoBehaviour {
 		}
 		mDataSnapshot = args.Snapshot;
 	}
-		
 }
