ff6522cc709e95a6b9492a1825ea2bd6e6b048c1
al285785 <al285785@uji.es>
2018-03-28 13:18:43 +0200

    Toon + errorres + inicio Tangram

---
+++ Assets/Scripts/PuzleTangram.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using Vuforia;
+
+public class PuzleTangram : MonoBehaviour {
+
+	[SerializeField] private Text correctText;
+	[SerializeField] private Text wrongText;
+	[SerializeField] private Text noVidasText;
+
+	// Use this for initialization
+	void Start () {
+		VuforiaBehaviour.Instance.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator ShowWrongText(){
+		wrongText.gameObject.SetActive (true);
+		yield return new WaitForSeconds (3.0f);
+		wrongText.gameObject.SetActive (false);
+	}
+
+	IEnumerator ShowCorrectText(){
+		correctText.gameObject.SetActive (true);
+		yield return new WaitForSeconds (3.0f);
+		SceneManager.LoadScene ("Vuforia");
+	}
+
+	IEnumerator ShowNoVidasText(){
+		noVidasText.gameObject.SetActive (true);
+		yield return new WaitForSeconds (3.0f);
+		WindowsManager.penalized = true;
+		SceneManager.LoadScene ("Vuforia");
+	}
+}
